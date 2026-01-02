4e5f12d5c3fe3e76a021e237d23603d7f0ae71b4
ptr8 <ptr8@cdc.gov>
2026-01-02 09:42:43 -0700

    cleanup removing services, common and getset

--- source-code/mmria/mmria.common/couchdb/replication_request.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace mmria.common.model.couchdb;
-public sealed class replication_request
-{
-    public replication_request(){}
-    public string source {get;set;}
-    public string target {get;set;}
-}
