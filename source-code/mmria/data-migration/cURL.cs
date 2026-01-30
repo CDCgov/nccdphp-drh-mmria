807a87aadabb4c3b8695639588e9791d9046a934
Nicholas LaBoffe <tyd7@cdc.gov>
2026-01-30 10:08:22 -0500

    major cleanup and refactoring. moved services and common into the repo. updated refs and docker files

--- source-code/mmria/data-migration/cURL.cs
+++
@@ -1,8 +0,0 @@
-// Type alias for canonical cURL in mmria.getset namespace
-// This allows code in the migrate namespace to use cURL without changing all usages
-using global::mmria.getset;
-
-namespace migrate
-{
-    using cURL = global::mmria.getset.cURL;
-}
